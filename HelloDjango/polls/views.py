4d275241e59415d8653eb647b89bc627d7976559
JaehunYoon <goodasd123@gmail.com>
2018-11-06 20:30:15 +0900

    View in Django

--- HelloDjango/polls/views.py
+++ HelloDjango/polls/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}")
+
+
+def results(self, question_id):
+    res = "You're looking at the results of question %s"
+    return HttpResponse(res % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}.")
+
+
 def index(request):
-    return HttpResponse("h4lo world~~")
+    return HttpResponse("h4lo world~~~")
